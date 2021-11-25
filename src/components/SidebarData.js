bbb6abe881a0718ae47049c19b5fd4ccdc52f802
DongShanHu <ken83924@gmail.com>
2021-11-25 22:42:18 +0800

    add sidebar without certern css

---
+++ src/components/SidebarData.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+
+export const SidebarData = [
+  { title: "Home", icons: "<HomeIcon/>", link: "/home" },
+  { title: "Analytics", icons: "<AssessmentIcon/>", link: "/analytics" },
+  { title: "Friends", icons: "<DashboardIcon/>", link: "/friends" },
+  { title: "Group", icons: "<GroupIcon/>", link: "/group" },
+];
